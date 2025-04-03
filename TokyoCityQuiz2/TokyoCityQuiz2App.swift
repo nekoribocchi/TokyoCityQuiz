8542ad7aacd076b23db099a610ccf22f10e9069a
nekoribocchi <nekoribocchi@gmail.com>
2025-04-04 00:04:24 +0900

    Delete Onbording

--- TokyoCityQuiz2/TokyoCityQuiz2App.swift
+++ TokyoCityQuiz2/TokyoCityQuiz2App.swift
@@ -2,14 +2,13 @@
 //  TokyoCityQuiz2App.swift
 //  TokyoCityQuiz2
 //
-//  Created by 羽田野真央 on 2025/02/04.
+//  Created by nekoribocchi on 2025/02/04.
 //
 
 import SwiftUI
 
 @main
 struct TokyoCityQuiz2App: App {
-    @State private var hasCompletedOnboarding: Bool = UserDefaults.standard.bool(forKey: "hasCompletedOnboarding")
     @StateObject private var settingViewModel = SettingViewModel()
     @StateObject private var quizViewModel: QuizViewModel
     
@@ -19,18 +18,11 @@ struct TokyoCityQuiz2App: App {
         _settingViewModel = StateObject(wrappedValue: settingVM)
         _quizViewModel = StateObject(wrappedValue: QuizViewModel())
     }
-
+    
     var body: some Scene {
         WindowGroup {
-            if hasCompletedOnboarding {
-                MainView(quizViewModel: quizViewModel)
-                    .environmentObject(settingViewModel)
-            } else {
-                OnboardingView(onboardingCompleted: {
-                    UserDefaults.standard.set(true, forKey: "hasCompletedOnboarding")
-                    hasCompletedOnboarding.toggle()
-                })
-            }
+            MainView(quizViewModel: quizViewModel)
+                .environmentObject(settingViewModel)
         }
     }
 }
