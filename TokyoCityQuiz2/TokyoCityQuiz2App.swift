a7552b18dc9cdd69f5ace5d53a5c0062ddd2f3aa
nekoribocchi <nekoribocchi@gmail.com>
2025-04-04 15:21:40 +0900

    Fixed App.swift

--- TokyoCityQuiz2/TokyoCityQuiz2App.swift
+++ TokyoCityQuiz2/TokyoCityQuiz2App.swift
@@ -10,14 +10,7 @@ import SwiftUI
 @main
 struct TokyoCityQuiz2App: App {
     @StateObject private var settingViewModel = SettingViewModel()
-    @StateObject private var quizViewModel: QuizViewModel
-    
-    init() {
-        // SettingViewModelを先に生成してquestionCountを取得
-        let settingVM = SettingViewModel()
-        _settingViewModel = StateObject(wrappedValue: settingVM)
-        _quizViewModel = StateObject(wrappedValue: QuizViewModel())
-    }
+    @StateObject private var quizViewModel = QuizViewModel()
     
     var body: some Scene {
         WindowGroup {
@@ -26,4 +19,3 @@ struct TokyoCityQuiz2App: App {
         }
     }
 }
-
